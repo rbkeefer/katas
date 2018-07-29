621a763c9cc64b976df8b102251cfe6d1116a049
Rob Keefer <rob.keefer@pomiet.com>
2018-07-29 08:31:58 -0400

    Update math

--- lib/math.ex
+++ lib/math.ex
@@ -1,10 +1,25 @@
 defmodule Math do
-  def sum(n) when n == 0, do: n
-  def sum(n) when n > 0, do: sum(n - 1) + n
-  def sum(n) when n < 0, do: sum(n + 1) + n
+  def sum_up(n) when n == 0, do: n
+  def sum_up(n) when n > 0, do: sum_up(n - 1) + n
+  def sum_up(n) when n < 0, do: sum_up(n + 1) + n
+
+  def sum_list([]), do: 0
+  def sum_list([head | tail]), do: sum_list(tail) + head
+
+  def mapsum([], _func), do: 0
+  def mapsum([head | tail], func), do: mapsum(tail, func) + func.(head)
 
   def gcd(x, y) when x < 0 or y < 0, do: "x and y must be positive"
   def gcd(x, y) when x == 0, do: y
   def gcd(x, y) when y == 0, do: x
   def gcd(x, y), do: gcd(y, rem(x, y))
+
+  def max(list), do: _max(list, -100_000_000_000)
+  def _max([], bigno), do: bigno
+  def _max([head | tail], bigno) when bigno > head, do: _max(tail, bigno)
+  def _max([head | tail], _bigno), do: _max(tail, head)
+
+  def span(from, to) when to < from, do: "to must be greater than from"
+  def span(from, to) when from == to, do: [to]
+  def span(from, to), do: [from | span(from + 1, to)]
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -1,10 +1,37 @@
 defmodule MathTest do
   use ExUnit.Case
 
-  test "Summer" do
-    assert 3 = Math.sum(2)
-    assert 15 = Math.sum(5)
-    assert -15 = Math.sum(-5)
+  test "Sum up to given value" do
+    assert 3 = Math.sum_up(2)
+    assert 15 = Math.sum_up(5)
+    assert -15 = Math.sum_up(-5)
+  end
+
+  test "Sum all values in a given list" do
+    assert 6 = Math.sum_list([1, 2, 3])
+    assert 15 = Math.sum_list([1, 2, 3, 4, 5])
+    assert 65 = Math.sum_list([11, 12, 13, 14, 15])
+  end
+
+  test "mapsum" do
+    assert 12 = Math.mapsum([1, 2, 3], &(&1 + &1))
+    assert 14 = Math.mapsum([1, 2, 3], &(&1 * &1))
+    assert 55 = Math.mapsum([1, 2, 3, 4, 5], &(&1 * &1))
+    assert 0 = Math.mapsum([11, 12, 13, 14, 15], &(&1 - &1))
+  end
+
+  test "max list" do
+    assert 3 = Math.max([1, 2, 3])
+    assert 100 = Math.max([100, 20, 3])
+    assert 20 = Math.max([10, 20, 3])
+    assert 0 = Math.max([0, -20, -3])
+    assert -3 = Math.max([-100, -20, -3])
+  end
+
+  test "generate span list" do
+    assert [0, 1, 2, 3] = Math.span(0, 3)
+    assert [10, 11, 12, 13] = Math.span(10, 13)
+    assert [-10, -9, -8, -7, -6, -5] = Math.span(-10, -5)
   end
 
   test "greatest common divisor" do
